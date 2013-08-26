62b648d6515e4e7419cbfa3fb9f5aff361584b98
Martin Foo <i25ffz@hotmail.com>
2013-08-26 16:01:53 +0800

    Use OpenSLES to play music.

--- jni/Android.mk
+++ jni/Android.mk
@@ -11,7 +11,7 @@ LOCAL_SRC_FILES := \
 	butterflygroup.cpp  demo.cpp    font.cpp    model.cpp  sky.cpp    texture.cpp  utility.cpp \
 	application.cpp
 
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv1_CM -lOpenSLES
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 
 include $(BUILD_SHARED_LIBRARY)
