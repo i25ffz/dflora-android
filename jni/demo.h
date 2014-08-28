b049818ddefc5cb1d6f92a4fdcf9c6d4bba92559
Martin Foo <i25ffz@hotmail.com>
2014-08-28 11:08:42 +0800

    Fix compile error.

--- jni/demo.h
+++ jni/demo.h
@@ -5,17 +5,17 @@ This source file is part of Dflora (Dancing Flora) OpenGL|ES 1.1 version
 Copyright (c) 2004-2005 Changzhi Li < richardzlee@163.com >
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free 
-Software Foundation; either version 2 of the License, or (at your option) any 
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option) any
 later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
 details.
 
-You should have received a copy of the GNU Lesser General Public License along 
-with this program; if not, write to the Free Software Foundation, Inc., 59 
+You should have received a copy of the GNU Lesser General Public License along
+with this program; if not, write to the Free Software Foundation, Inc., 59
 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to
 http://www.gnu.org/copyleft/lesser.txt.
 -----------------------------------------------------------------------------
@@ -24,7 +24,9 @@ http://www.gnu.org/copyleft/lesser.txt.
 #ifndef _DEMO_H_
 #define _DEMO_H_
 
-//#include <windows.h>
+#ifndef ANDROID
+#include <windows.h>
+#endif
 #include <math.h>
 #include <GLES/gl.h>
 #include <EGL/egl.h>
@@ -58,7 +60,7 @@ public:
 	bool Create();
 	void Destroy();
 
-	void Tick(TimeValue dt);	
+	void Tick(TimeValue dt);
 	void Render();
 
 	bool IsFinished() { return m_bFinished; }
@@ -108,17 +110,17 @@ private:
 	Interval m_itvFlower1Dance, m_itvFlower2Dance, m_itvFlower3Dance, m_itvFlower4Dance;
 	Interval m_itvCameraAnimation1, m_itvCameraAnimation2;
 	Interval m_itvButterflyGroupAnimation;
-	
+
 	// rendering interval
 	Interval m_itvFlowersRender;
 	Interval m_itvTitleFontRender, m_itvAuthorFontRender;
 
 	Interval m_itvLensFlareRender;
-	
+
 	// butterfly Render
 	Interval m_itvButterflyGroupRender1;
 	Interval m_itvButterflyGroupRender2;
-	
+
 	// Camera fade
 	Interval m_itvCameraFadeIn, m_itvCameraFadeOut;
 	Interval m_itvCameraFadeInMid, m_itvCameraFadeOutMid;
